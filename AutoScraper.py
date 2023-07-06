2294b64d4e63207437056dea9dcf103110f1c30b
taidavis1 <73322981+taidavis1@users.noreply.github.com>
2023-07-05 20:19:18 -0700

    Update AutoScraper.py

--- AutoScraper.py
+++ AutoScraper.py
@@ -11,6 +11,8 @@ class Auto_Scraper():
         self.result_list = []
         self.prompt = prompt
         self.url = ""
+        self.index = 1
+        self.jump = 20
     def start_search(self):
         self.driver.get("https://www.google.com/")
         element = self.driver.find_element(By.XPATH , '//*[@id="APjFqb"]')
@@ -21,21 +23,21 @@ class Auto_Scraper():
         self.url = self.driver.current_url
         print("Search Completed! ")
         time.sleep(2)
-    def check_phone_nums(self , index):                      
+    def check_phone_nums(self):                      
         try:                                                   
-            phone_nums = self.driver.find_element(By.XPATH , f'//*[@id="yDmH0d"]/c-wiz/div/div[3]/div/div/div[1]/div[3]/div[3]/c-wiz/div/div/div[1]/c-wiz/div/div[{index}]/div[1]/div/div/div/div[2]/div[3]/span[3]')
+            phone_nums = self.driver.find_element(By.XPATH , f'//*[@id="yDmH0d"]/c-wiz/div/div[3]/div/div/div[1]/div[3]/div[3]/c-wiz/div/div/div[1]/c-wiz/div/div[{self.index}]/div[1]/div/div/div/div[2]/div[3]/span[3]')
             return phone_nums.text
         except NoSuchElementException:            
             return "False"                        
-    def check_data(self , index):                 
+    def check_data(self):                 
         try: 
             time.sleep(1)                          
-            self.driver.find_element(By.XPATH , f'//*[@id="yDmH0d"]/c-wiz/div/div[3]/div/div/div[1]/div[3]/div[3]/c-wiz/div/div/div[1]/c-wiz/div/div[{index}]/div[2]/div/div/div/div/div/div[1]/a/div/div/button/span[2]')
+            self.driver.find_element(By.XPATH , f'//*[@id="yDmH0d"]/c-wiz/div/div[3]/div/div/div[1]/div[3]/div[3]/c-wiz/div/div/div[1]/c-wiz/div/div[{self.index}]/div[2]/div/div/div/div/div/div[1]/a/div/div/button/span[2]')
             return True
         except NoSuchElementException:
             try:
-                title = self.driver.find_element(By.XPATH , f'//*[@id="yDmH0d"]/c-wiz/div/div[3]/div/div/div[1]/div[3]/div[3]/c-wiz/div/div/div[1]/c-wiz/div/div[{index}]/div[1]/div/div/div/div[2]/div[1]/div')
-                phone_number = self.check_phone_nums(index=index)
+                title = self.driver.find_element(By.XPATH , f'//*[@id="yDmH0d"]/c-wiz/div/div[3]/div/div/div[1]/div[3]/div[3]/c-wiz/div/div/div[1]/c-wiz/div/div[{self.index}]/div[1]/div/div/div/div[2]/div[1]/div')
+                phone_number = self.check_phone_nums()
                 if phone_number != "False":
                     data = {                                      
                         "Name " : title.text,
@@ -57,21 +59,21 @@ class Auto_Scraper():
             return True
         except NoSuchElementException:
             return False
-    def data_loop(self , index , jump):
-        while index:
-            case = self.check_data(index)
+    def data_loop(self):
+        while self.index:
+            case = self.check_data()
             if not case:
-                index = 1
-                self.driver.get(self.url + "&lci=" + str(jump))
+                self.index = 1
+                self.driver.get(self.url + "&lci=" + str(self.jump))
                 time.sleep(1)
                 if not self.check_btn():
                     print("Loop Complete Done !")
                     self.driver.close()
                     self.driver.quit()
                     break
-                jump += 20
+                self.jump += 20
                 continue
-            index += 2
+            self.index += 2
     def print_data(self):
         for i in self.result_list:
             print(i)
