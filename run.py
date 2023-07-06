63d7a062cd25e218fd7bce33653b33e7944c6603
taidavis1 <73322981+taidavis1@users.noreply.github.com>
2023-07-05 20:07:12 -0700

    Update run.py

--- run.py
+++ run.py
@@ -2,11 +2,11 @@ from AutoScraper import Auto_Scraper
 from Data_Cleaning import Data_Cleaning
 
 if __name__ == '__main__':
-    scrapper = Auto_Scraper(prompt="nails salon in Merced")
+    scrapper = Auto_Scraper(prompt="{prompt}")
     scrapper.start_search()
     index = 1
     jump = 20
     data = scrapper.data_loop(index , jump)
     scrapper.print_data()
-    data = Data_Cleaning(scrapper.result_list , 'C:/Users/V3 DESKTOP 3/Desktop/All Proejcts/Python learning/Web_Scraping/csv_files/data.csv')
+    data = Data_Cleaning(scrapper.result_list , '{file_path}')
     data.Change_To_Csv()
