1992f0acf2e29d43c67604ce18b05c3c0a73d625
taidavis1 <73322981+taidavis1@users.noreply.github.com>
2023-07-05 20:19:52 -0700

    Update run.py

--- run.py
+++ run.py
@@ -4,9 +4,7 @@ from Data_Cleaning import Data_Cleaning
 if __name__ == '__main__':
     scrapper = Auto_Scraper(prompt="{prompt}")
     scrapper.start_search()
-    index = 1
-    jump = 20
-    data = scrapper.data_loop(index , jump)
+    data = scrapper.data_loop()
     scrapper.print_data()
     data = Data_Cleaning(scrapper.result_list , '{file_path}')
     data.Change_To_Csv()
